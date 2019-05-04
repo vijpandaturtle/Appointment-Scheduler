1362b7c8721d0383ea777eaecf8cc00e1b7cdbfe
Vijayasri Iyer <thisisvij98@gmail.com>
2019-05-04 23:48:29 +0530

    Modified fields

--- appointments/project_app/models.py
+++ appointments/project_app/models.py
@@ -10,15 +10,9 @@ class appointments(models.Model):
 	appointment_description = models.TextField()
 	time_field = models.TimeField(blank=True, null=True)
 	date_field = models.DateField(blank=True, null=True)
-	address = models.CharField(max_length=250)
-	state = models.CharField(max_length=5)
-	city = models.CharField(max_length=50)
-	zip_code = models.CharField(max_length=6)
 	phone = models.CharField(max_length=14)
 	notes = models.TextField()
 
 
 	def __str__(self):
 		return self.appointment_title
-
-
